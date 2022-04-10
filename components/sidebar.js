816880e0f1bfbb7f417e582a702698df7de0ec4f
Rahul.B <talktorahul.b@gmail.com>
2022-04-10 12:27:23 +0530

    cube is modified some error need to be rectified

---
+++ components/sidebar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import glowingCube from './sidebar/glowingCube'
+
+
+const sidebar = () => {
+  return (
+    <div><glowingCube /></div>
+  )
+}
+
+export default sidebar
