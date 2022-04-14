9d72c7b4ef7b40a72160069ca8d57e072c56fb53
sabari245 <sabari2004@hotmail.com>
2022-04-14 16:24:38 +0530

    tailwind added; class convertion going on

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,59 +1,97 @@
 import React from "react";
-import Logo from "./Logo";
 import styles from "./NavBar.module.css";
-import Contact from "./Contact";
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaTwitter,
+  FaYoutube,
+  FaBars,
+} from "react-icons/fa";
 
-const handleClick = () =>{
-    var x = document.getElementById("NavBar");
-    if(x.style.height === "10vh"){
-      x.setAttribute("style","height:100vh");
-    }
-    else{
-      x.setAttribute("style","height:10vh");
-    }
+const handleClick = () => {
+  var x = document.getElementById("NavBar");
+  if (x.style.height === "10vh") {
+    x.setAttribute("style", "height:100vh");
+  } else {
+    x.setAttribute("style", "height:10vh");
+  }
+};
+
+/*
+
+.nav .toggle .toggleIcons{
+    display: flex;
+    justify-content: flex-end;
+    padding: 1.7rem 0;
 }
- 
+
+.nav .toggle .toggleIcons i{
+    font-size: 1.4rem;
+    color:#3f4954;
+} */
 
 const Sidebar = () => {
   return (
-    <div className={styles.body}>
-    <nav className={styles.nav} id="NavBar">
-      <div className={styles.menu}>
-        <div className={styles.brand}>
-          <a href="#" className="text-gray">Dash Blog</a>
-        </div>
-        <div className={styles.toggle}>
-          <div className={styles.toggleIcons}>
-            <i className="fa fas fa-bars" onClick={handleClick}></i>
+    <div>
+      <nav
+        className="bg-white transition-[height] ease-in-out delay-1000 ml-5 overflow-hidden"
+        style={{ height: "10vh" }}
+        id="NavBar"
+      >
+        {" "}
+        {/* .nav */}
+        <div className="flex flex-row justify-between flex-wrap">
+          {" "}
+          {/* .menu */}
+          <div className={styles.brand}>
+            <a href="#" className="text-gray">
+              Dash Blog
+            </a>
+          </div>
+          <div className="absolute top-0 width-[90%] cursor-pointer hidden">
+            {" "}
+            {/* .toggle */}
+            <div className="flex justify-end px-[1.7rem]" onClick={handleClick}>
+              {" "}
+              {/* .toggleIcons */}
+              <FaBars />
+            </div>
+          </div>
+          <div>
+            <ul className={styles.items}>
+              <li className={styles.link}>
+                <a href="#"> Home </a>
+              </li>
+              <li className={styles.link}>
+                <a href="#"> Blogs </a>
+              </li>
+              <li className={styles.link}>
+                <a href="#"> Services </a>
+              </li>
+              <li className={styles.link}>
+                <a href="#"> About </a>
+              </li>
+              <li className={styles.link}>
+                <a href="/Contact"> Contact </a>
+              </li>
+            </ul>
+          </div>
+          <div className={styles.socialText}>
+            <a href="#">
+              <FaFacebookF />
+            </a>
+            <a href="#">
+              <FaInstagram />
+            </a>
+            <a href="#">
+              <FaTwitter />
+            </a>
+            <a href="#">
+              <FaYoutube />
+            </a>
           </div>
         </div>
-        <div>
-          <ul className={styles.items}>
-            <li className={styles.link}>
-              <a href = "#"> Home </a>
-            </li>
-            <li className={styles.link}>
-              <a href = "#"> Blogs </a>
-            </li>
-            <li className={styles.link}>
-              <a href = "#"> Services </a>
-            </li>
-            <li className={styles.link}>
-              <a href = "#"> About </a>
-            </li>
-            <li className={styles.link}>
-              <a href = "/Contact"> Contact </a>
-            </li>
-          </ul>
-        </div>
-        <div className={styles.socialText}>
-             <a href="#"><i className="fa fa-brands fa-facebook-f"></i></a>
-             <a href="#"><i className="fa fa-brands fa-instagram"></i></a>
-             <a href="#"><i className="fa fa-brands fa-twitter"></i></a>
-             <a href="#"><i className="fa fa-brands fa-youtube"></i></a>
-        </div>
-      </div>
-    </nav>
+      </nav>
     </div>
   );
 };
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        pop: ["Poppins", "sans-serif"],
+        mont: ["Montserrat", "sans-serif"],
+        har: ["Harmattan", "sans-serif"],
+        bree: ["Bree Serif", "serif"],
+        liv: ["Livvic", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
